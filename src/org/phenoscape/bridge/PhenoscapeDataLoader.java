d00aed5fd6f1eae2335cf4fa76da12953f5d8f5a
Cartik Kothari <cartik.kothari@duke.edu>
2008-11-13 22:38:01 +0000

    Added code block to read in db connection parameters from a configuration file

--- src/org/phenoscape/bridge/PhenoscapeDataLoader.java
+++ src/org/phenoscape/bridge/PhenoscapeDataLoader.java
@@ -1,6 +1,8 @@
 package org.phenoscape.bridge;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Arrays;
@@ -9,6 +11,8 @@ import java.util.List;
 
 import org.apache.xmlbeans.XmlException;
 import org.obd.model.Graph;
+import org.obd.query.Shard;
+import org.obd.query.impl.AbstractSQLShard;
 import org.obd.query.impl.OBDSQLShard;
 import org.phenoscape.io.NeXMLReader;
 import org.phenoscape.model.DataSet;
@@ -21,30 +25,38 @@ public class PhenoscapeDataLoader {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//String dataDir = args[0];
+		// String dataDir = args[0];
 		PhenoscapeDataLoader pdl = new PhenoscapeDataLoader();
-		try{
+		try {
 			pdl.loadData(args[0]);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void loadData(String dataDir) throws XmlException, IOException, SQLException,
-			ClassNotFoundException {
+	public void loadData(String dataDir) throws XmlException, IOException,
+			SQLException, ClassNotFoundException {
 		OntologyController oc = new OntologyController();
 		OBDModelBridge bridge = new OBDModelBridge();
 		NeXMLReader reader;
 		DataSet ds;
 		Graph g;
-		//String basePath = "/home/cartik/data/";
+		// String basePath = "/home/cartik/data/";
 		File baseDataDir = new File(dataDir);
 		int i = 0;
 		// OBOSessionShard s = new OBOSessionShard();
-		OBDSQLShard obdsql = new OBDSQLShard();
-		obdsql.connect("?", "?",
-				"?");
+		File connParamFile = new File("testfiles/connectionParameters");
+		BufferedReader br = new BufferedReader(new FileReader(connParamFile));
+		String[] connParams = new String[3];
+		String param;
+		int j = 0;
+		while ((param = br.readLine()) != null) {
+			connParams[j++] = param;
+		}
+
+		Shard obdsql = new OBDSQLShard();
+		((AbstractSQLShard) obdsql).connect(connParams[0], connParams[1],
+				connParams[2]);
 		List<File> baseDirs = Arrays.asList(baseDataDir.listFiles());
 		Collections.sort(baseDirs);
 		for (File baseDir : baseDirs) {

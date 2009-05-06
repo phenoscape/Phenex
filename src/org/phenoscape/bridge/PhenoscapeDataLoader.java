fdbbf073824726f1b920e3d6aacc288bd99c495d
Jim Balhoff <balhoff@nescent.org>
2009-05-06 15:01:20 +0000

    Some cleanups to PhenoscapeDataLoader for use with Ant build file for OBD.  Parameters are passed as system properties. Data files are found recursively. Changed logging to use log4j.

--- src/org/phenoscape/bridge/PhenoscapeDataLoader.java
+++ src/org/phenoscape/bridge/PhenoscapeDataLoader.java
@@ -1,13 +1,9 @@
 package org.phenoscape.bridge;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.apache.log4j.BasicConfigurator;
@@ -16,7 +12,6 @@ import org.apache.xmlbeans.XmlException;
 import org.bbop.dataadapter.DataAdapterException;
 import org.obd.model.Graph;
 import org.obd.query.Shard;
-import org.obd.query.impl.AbstractSQLShard;
 import org.obd.query.impl.OBDSQLShard;
 import org.obo.dataadapter.OBOAdapter;
 import org.obo.dataadapter.OBOFileAdapter;
@@ -26,74 +21,61 @@ import org.phenoscape.model.DataSet;
 
 public class PhenoscapeDataLoader {
 
-	/**
-	 * @param args
-	 */
+    /** The db-host system property should contain the name of the database server. */
+    public static final String DB_HOST = "db-host";
+    /** The db-name system property should contain the name of the database. */
+    public static final String DB_NAME = "db-name";
+    /** The db-user system property should contain the database username. */
+    public static final String DB_USER = "db-user";
+    /** The db-password system property should contain the database password. */
+    public static final String DB_PASSWORD = "db-password";
+    /** The ontology-dir system property should contain the path to a folder with ontologies to be loaded. */
+    public static final String ONTOLOGY_DIR = "ontology-dir";
+    
+    private Shard shard;
+    private OBOSession session;
 
-	public static void main(String[] args) {
-		// String dataDir = args[0];
-		BasicConfigurator.configure();
-		PhenoscapeDataLoader pdl = new PhenoscapeDataLoader();
-		try {
-			pdl.loadData(args[0], args[1]);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public PhenoscapeDataLoader() throws SQLException, ClassNotFoundException {
+        this.shard = this.initializeShard();
+        this.session = this.loadOBOSession();
+    }
 
-	public void loadData(String dataDir, String dbConn) throws XmlException, IOException,
-			SQLException, ClassNotFoundException {
-		OBDModelBridge bridge;
-		NeXMLReader reader;
-		DataSet ds;
-		Graph g;
-		// String basePath = "/home/cartik/data/";
-		File baseDataDir = new File(dataDir);
-		int i = 0;
-		// OBOSessionShard s = new OBOSessionShard();
-		File connParamFile = new File(dbConn);
-		BufferedReader br = new BufferedReader(new FileReader(connParamFile));
-		String[] connParams = new String[3];
-		String param;
-		int j = 0, t = 0;
-		while ((param = br.readLine()) != null) {
-			connParams[j++] = param;
-		}
+    public static void main(String[] args) throws XmlException, IOException, SQLException, ClassNotFoundException {
+        BasicConfigurator.configure();
+        PhenoscapeDataLoader pdl = new PhenoscapeDataLoader();
+        pdl.processDataFolder(new File(args[0]));
+    }
 
-		Shard obdsql = new OBDSQLShard();
-		((AbstractSQLShard) obdsql).connect(connParams[0], connParams[1],
-				connParams[2]);
-		final OBOSession oboSession = this.loadOBOSession();
-		List<File> baseDirs = Arrays.asList(baseDataDir.listFiles());
-		Collections.sort(baseDirs);
-		for (File baseDir : baseDirs) {
-			// check to avoid .. and . directories
-			if (baseDir.isDirectory() && !baseDir.getName().contains(".")) {
-				List<File> files = Arrays.asList(baseDir.listFiles());
-				Collections.sort(files);
-				for (File dataFile : files) {
-					// another check to avoid directories
-					if (dataFile.isFile()) {
-						System.out.println(++i + ". Started work with "
-								+ dataFile.getAbsolutePath());
-						reader = new NeXMLReader(dataFile, oboSession);
-						ds = reader.getDataSet();
-						bridge = new OBDModelBridge();
-						g = bridge.translate(ds, dataFile);
-						obdsql.putGraph(g);
-						t += g.getStatements().size();
-						System.out.println(g.getStatements().size() + " records added");
-					}
-				}
-			}
-		}
-		System.out.println(t + " total statements from " + i + " files loaded. Done!");
-	}
-	
-	private OBOSession loadOBOSession() {
-	    final OBOFileAdapter fileAdapter = new OBOFileAdapter();
+    private void processDataFolder(File folder) {
+        for (File file : folder.listFiles()) {
+            if (file.isDirectory()) {
+                this.processDataFolder(file);
+            } else if (file.getName().endsWith(".xml")) {
+                try {
+                    this.processDataFile(file);
+                } catch (XmlException e) {
+                    log().error("Failed parsing " + file, e);
+                } catch (IOException e) {
+                    log().error("Failed reading " + file, e);
+                }
+            }
+        }
+    }
+
+    private void processDataFile(File file) throws XmlException, IOException {
+        log().info("Started work with " + file.getAbsolutePath());
+        NeXMLReader reader = new NeXMLReader(file, this.session);
+        DataSet ds = reader.getDataSet();
+        OBDModelBridge bridge = new OBDModelBridge();
+        Graph g = bridge.translate(ds, file);
+        this.shard.putGraph(g);
+        log().info(g.getStatements().size() + " records added");
+    }
+
+    private OBOSession loadOBOSession() {
+        final OBOFileAdapter fileAdapter = new OBOFileAdapter();
         OBOFileAdapter.OBOAdapterConfiguration config = new OBOFileAdapter.OBOAdapterConfiguration();
-        config.setReadPaths(this.getPaths());
+        config.setReadPaths(this.getOntologyPaths());
         config.setBasicSave(false);
         config.setAllowDangling(true);
         config.setFollowImports(false);
@@ -103,20 +85,25 @@ public class PhenoscapeDataLoader {
             log().fatal("Failed to load ontologies", e);
             return null;
         }
-	}
+    }
 
-    private List<String> getPaths() {
-        // return an array of paths to OBO files, as strings
-    	List<String> paths = new ArrayList<String>();
-    	File ontCache = new File("Ontology-Cache");
-    	for(File f : ontCache.listFiles()){
-    		paths.add(f.getAbsolutePath());
-    	}
+    private List<String> getOntologyPaths() {
+        List<String> paths = new ArrayList<String>();
+        File ontCache = new File(System.getProperty(ONTOLOGY_DIR));
+        for (File f : ontCache.listFiles()) {
+            paths.add(f.getAbsolutePath());
+        }
         return paths;
     }
-    
+
+    private Shard initializeShard() throws SQLException, ClassNotFoundException {
+        OBDSQLShard obdsql = new OBDSQLShard();
+        obdsql.connect("jdbc:postgresql://" + System.getProperty(DB_HOST) + "/" + System.getProperty(DB_NAME), System.getProperty(DB_USER), System.getProperty(DB_PASSWORD));
+        return obdsql;
+    }
+
     private Logger log() {
         return Logger.getLogger(this.getClass());
     }
-    
+
 }

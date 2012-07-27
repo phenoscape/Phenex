408f53a19bb9116b3152dde6b384538eab59a8e8
Jim Balhoff <balhoff@nescent.org>
2012-07-27 11:47:13 -0400

    Added specific version to Phenoscape anatomy file.

--- src/org/obo/annotation/base/UserOntologyConfiguration.java
+++ src/org/obo/annotation/base/UserOntologyConfiguration.java
@@ -23,7 +23,7 @@ public class UserOntologyConfiguration implements OntologyConfiguration {
     private static final List<OntologySource> defaultSources = new ArrayList<OntologySource>();
     static {
         try {
-            defaultSources.add(new OntologySource("Phenoscape Anatomy", new URL("http://phenoscape.svn.sourceforge.net/viewvc/phenoscape/trunk/vocab/phenoscape-anatomy.obo")));
+            defaultSources.add(new OntologySource("Phenoscape Anatomy", new URL("http://phenoscape.svn.sourceforge.net/viewvc/phenoscape/trunk/vocab/phenoscape-anatomy.obo?revision=3552")));
             defaultSources.add(new OntologySource("Vertebrate Taxonomy", new URL("http://phenoscape.svn.sourceforge.net/viewvc/phenoscape/trunk/vocab/vertebrate_taxonomy.obo")));
             defaultSources.add(new OntologySource("Museum Collections", new URL("http://phenoscape.svn.sourceforge.net/viewvc/*checkout*/phenoscape/trunk/vocab/fish_collection_abbreviation.obo")));
             defaultSources.add(new OntologySource("Phenotypic Qualities", new URL("http://obo.cvs.sourceforge.net/*checkout*/obo/obo/ontology/phenotype/quality.obo")));

bc8bb93012980a33c1e23ef963ec45d2cb4e4635
Jim Balhoff <balhoff@nescent.org>
2013-03-04 14:45:20 -0600

    Initial implementation of Consistency Panel. Several consistency checks remain to be implemented.

---
+++ src/org/phenoscape/util/AnnotationConsistencyChecker.java
@@ -0,0 +1,69 @@
+package org.phenoscape.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.obo.datamodel.LinkedObject;
+import org.obo.datamodel.OBOClass;
+import org.obo.datamodel.OBOProperty;
+import org.obo.datamodel.OBOSession;
+import org.obo.datamodel.TermSubset;
+import org.obo.filters.LinkFilter;
+import org.obo.filters.LinkFilterImpl;
+import org.obo.util.TermUtil;
+import org.phenoscape.model.Character;
+import org.phenoscape.model.Phenotype;
+import org.phenoscape.model.State;
+
+public class AnnotationConsistencyChecker {
+
+	private final OBOSession session;
+	private TermSubset relation_slim = null;
+	private static final String STRUCTURE = "PATO:0000141";
+	private static final String POSITION = "PATO:0000140";
+	private static final String SIZE = "PATO:0000117";
+
+	public AnnotationConsistencyChecker(OBOSession session) {
+		this.session = session;
+		for (TermSubset subset : session.getSubsets()) {
+			if ((subset.getName() != null) && (subset.getName().equals("relational_slim"))) {
+				relation_slim = subset;
+				break;
+			}
+		}
+	}
+
+	public Collection<ConsistencyIssue> checkAnnotation(Character character, State state, Phenotype phenotype) {
+		final Collection<ConsistencyIssue> issues = new ArrayList<ConsistencyIssue>(); 
+		if (phenotype.getQuality() == null) {
+			issues.add(new ConsistencyIssue(character, state, "No quality has been entered."));
+		}
+		if (relation_slim != null) {
+			if ((phenotype.getQuality() != null) && (phenotype.getQuality().getSubsets() != null)) {
+				if (phenotype.getQuality().getSubsets().contains(relation_slim)) {
+					if (phenotype.getRelatedEntity() == null) {
+						issues.add(new ConsistencyIssue(character, state, "Relational quality has been used without a related entity."));
+					}
+				} else {
+					if ((phenotype.getRelatedEntity() != null) && (!this.isOptionallyRelationalQuality(phenotype.getQuality()))) {
+						issues.add(new ConsistencyIssue(character, state, "Related entity requires a relational quality."));
+					}
+				}
+			}
+
+		}
+		return issues;
+	}
+
+	private boolean isOptionallyRelationalQuality(OBOClass quality) {
+		if ((quality.getID().equals(STRUCTURE)) || (quality.getID().equals(POSITION))) {
+			return true;
+		} else {
+			final OBOClass size = (OBOClass)(session.getObject(SIZE));
+			final LinkFilter filter = new LinkFilterImpl((OBOProperty)(session.getObject("OBO_REL:is_a")));
+			final Collection<LinkedObject> sizes = TermUtil.getDescendants(size, true, filter);
+			return sizes.contains(quality);
+		}
+	}
+
+}
---
+++ src/org/phenoscape/view/ConsistencyReviewComponent.java
@@ -0,0 +1,127 @@
+package org.phenoscape.view;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.util.Comparator;
+
+import javax.swing.AbstractAction;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JToolBar;
+
+import org.obo.app.swing.BugWorkaroundTable;
+import org.phenoscape.controller.PhenexController;
+import org.phenoscape.model.Character;
+import org.phenoscape.model.Phenotype;
+import org.phenoscape.model.State;
+import org.phenoscape.util.AnnotationConsistencyChecker;
+import org.phenoscape.util.ConsistencyIssue;
+
+import ca.odell.glazedlists.BasicEventList;
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.GlazedLists;
+import ca.odell.glazedlists.gui.AdvancedTableFormat;
+import ca.odell.glazedlists.swing.EventTableModel;
+
+import com.eekboom.utils.Strings;
+
+public class ConsistencyReviewComponent extends PhenoscapeGUIComponent {
+
+	private ConsistencyTableFormat tableFormat;
+	private JTable table;
+	private JButton refreshButton;
+	private EventList<ConsistencyIssue> issues = new BasicEventList<ConsistencyIssue>();
+	private AnnotationConsistencyChecker checker;
+
+	public ConsistencyReviewComponent(String id, PhenexController controller) {
+		super(id, controller);
+		this.initializeInterface();
+	}
+
+	private void initializeInterface() {
+		this.checker = new AnnotationConsistencyChecker(this.getController().getOntologyCoordinator().getOBOSession());
+		this.setLayout(new BorderLayout());
+		this.tableFormat = new ConsistencyTableFormat();
+		final EventTableModel<ConsistencyIssue> phenotypesTableModel = new EventTableModel<ConsistencyIssue>(this.issues, this.tableFormat);
+		this.table = new BugWorkaroundTable(phenotypesTableModel);
+		this.add(new JScrollPane(this.table), BorderLayout.CENTER);
+		this.add(this.createToolBar(), BorderLayout.NORTH);
+	}
+
+	private JToolBar createToolBar() {
+		final JToolBar toolBar = new JToolBar();
+		this.refreshButton = new JButton(new AbstractAction(null, new ImageIcon(this.getClass().getResource("/org/phenoscape/view/images/list-add.png"))) {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				refresh();
+			}
+		});
+		this.refreshButton.setToolTipText("Refresh");
+		toolBar.add(this.refreshButton);
+		toolBar.setFloatable(false);
+		return toolBar;
+	}
+
+	private void refresh() {
+		this.issues.clear();
+		for (Character character : this.getController().getDataSet().getCharacters()) {
+			for (State state : character.getStates()) {
+				for (Phenotype phenotype : state.getPhenotypes()) {
+					this.issues.addAll(checker.checkAnnotation(character, state, phenotype));
+				}
+			}
+		}
+	}
+
+	private class ConsistencyTableFormat implements AdvancedTableFormat<ConsistencyIssue> {
+
+		@Override
+		public int getColumnCount() {
+			return 4;
+		}
+
+		@Override
+		public String getColumnName(int column) {
+			switch (column) {
+			case 0: return "";
+			case 1: return "Character";
+			case 2: return "State";
+			case 3: return "Issue";
+			default: return null;
+			}
+		}
+
+		@Override
+		public Object getColumnValue(ConsistencyIssue issue, int column) {
+			switch (column) {
+			case 0: return getController().getDataSet().getCharacters().indexOf(issue.getCharacter()) + 1;
+			case 1: return issue.getCharacter().getLabel();
+			case 2: return issue.getState().getLabel();
+			case 3: return issue.getIssue();
+			default: return null;
+			}
+		}
+
+		@Override
+		public Class<?> getColumnClass(int column) {
+			if (column == 0) {
+				return Integer.class;
+			} else {
+				return String.class;	
+			}
+		}
+
+		@Override
+		public Comparator<?> getColumnComparator(int column) {
+			if (column == 0) {
+				return GlazedLists.comparableComparator();
+			} else {
+				return Strings.getNaturalComparator();	
+			}
+		}
+
+	}
+
+}

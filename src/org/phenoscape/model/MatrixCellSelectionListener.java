fa839cf6b3878d2d60a74128594b2c52b163313c
Jim Balhoff <balhoff@nescent.org>
2014-03-05 16:47:56 -0600

    Moved classes into maven structure.

--- src/org/phenoscape/model/MatrixCellSelectionListener.java
+++
@@ -1,9 +0,0 @@
-package org.phenoscape.model;
-
-import org.phenoscape.controller.PhenexController;
-
-public interface MatrixCellSelectionListener {
-	
-	public void matrixCellWasSelected(MatrixCell cell, PhenexController controller);
-
-}
